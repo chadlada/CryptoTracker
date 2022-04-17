2a2c70341a7d53bc495552f1ffa095d5e15aa6c6
chadlada <chadlada@gmail.com>
2022-04-16 21:30:39 -0400

    Added some static HTML and some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <header>
+        <header>Crypto Tracker</header>
+      </header>
+      <div className="instructions">
+        <p>
+          <strong>Instructions: </strong>Every 10 seconds crypto currency prices
+          will update{' '}
+        </p>
+      </div>
+    </main>
+  )
 }
